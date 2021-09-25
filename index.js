4b1b43e71b2dd1479de4bc366e1ddb22e4d66308
asror797 <asror797shahobiddinov@gmail.com>
2021-09-25 13:26:20 +0500

    first message

---
+++ index.js
@@ -0,0 +1,20 @@
+// function funksiya(a) {
+//   return a*a;
+// }
+
+// console.log(funksiya(45));
+
+a=5;
+
+// let count = 1;
+// while (count < 10) {
+//     console.log(count);
+//     count +=2;
+// }
+
+let counter_zero=0;
+
+while(counter==100) {
+  console.log(counter_zero);
+  counter_zero++
+}
